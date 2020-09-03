43b69f9589a15e7a22792f5bd573ee51bfba7ee3
ilyutoev <ilyutoev@gmail.com>
2020-09-03 21:50:03 +0300

    ОБработка всех ошибок бота и отправка в канал

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class LogsToTelegramHandler(logging.Handler):
         log_entry = self.format(record)
         bot.send_message(
           chat_id=chat_id, text=log_entry,
-          parse_mode=telegram.ParseMode.HTML,
           disable_web_page_preview=True
         )
 
@@ -42,32 +41,36 @@ logger.info('Devman notifications bot started.')
 
 while True:
     try:
-      response = requests.get(check_list_url, headers=headers, params=payload, timeout=100)
-      response.raise_for_status()
-    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-      continue
+        try:
+          response = requests.get(check_list_url, headers=headers, params=payload, timeout=100)
+          response.raise_for_status()
+        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
+          continue
 
-    response_dict = response.json()
-    status = response_dict.get('status')
-    if status == 'found':
-      for attempt in response_dict.get('new_attempts', []):
-        is_negative = attempt.get('is_negative')
-        result = "К сожалению в работе нашлись ошибки"
-        if is_negative is False:
-          result = "Преподавателю все понравилось, можно приступать к следующему уроку!"
+        response_dict = response.json()
+        status = response_dict.get('status')
+        if status == 'found':
+          for attempt in response_dict.get('new_attempts', []):
+            is_negative = attempt.get('is_negative')
+            result = "К сожалению в работе нашлись ошибки"
+            if is_negative is False:
+              result = "Преподавателю все понравилось, можно приступать к следующему уроку!"
 
-        lesson_title = attempt.get('lesson_title')
+            lesson_title = attempt.get('lesson_title')
 
-        lesson_url = attempt.get('lesson_url')
-        lesson_url = f'https://dvmn.org{lesson_url}'
-        
-        msg = f'У вас проверили работу <a href="{lesson_url}">{lesson_title}</a>\n\n{result}'
+            lesson_url = attempt.get('lesson_url')
+            lesson_url = f'https://dvmn.org{lesson_url}'
+            
+            msg = f'У вас проверили работу <a href="{lesson_url}">{lesson_title}</a>\n\n{result}'
 
-        bot.send_message(
-          chat_id=chat_id, text=msg,
-          parse_mode=telegram.ParseMode.HTML,
-          disable_web_page_preview=True
-        )
+            bot.send_message(
+              chat_id=chat_id, text=msg,
+              parse_mode=telegram.ParseMode.HTML,
+              disable_web_page_preview=True
+            )
 
-    elif status == 'timeout':
-      payload['timestamp'] = response_dict.get('timestamp_to_request')
+        elif status == 'timeout':
+          payload['timestamp'] = response_dict.get('timestamp_to_request')
+    except Exception as e:
+        logger.error('Бот упал')
+        logger.exception(e)
