e0de65d1f7e3adebe13330248da5ee7e4b961e5f
ilyutoev <ilyutoev@gmail.com>
2020-09-16 16:36:46 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class LogsToTelegramHandler(logging.Handler):
     def __init__(self, telegram_token, chat_id):
         super().__init__()
         self.chat_id = chat_id
-        log_bot = telegram.Bot(token=telegram_token)
+        self.log_bot = telegram.Bot(token=telegram_token)
 
     def emit(self, record):
         log_entry = self.format(record)
-        bot.send_message(
-          chat_id=self.chat_id, text=log_entry,
-          disable_web_page_preview=True
+        self.log_bot.send_message(
+            chat_id=self.chat_id, text=log_entry,
+            disable_web_page_preview=True
         )
 
 logger = logging.getLogger("devman_notifications_bot")
